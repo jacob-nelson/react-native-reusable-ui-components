19aa2898c7a9f6a92d87666fecded1fb09d5cc38
Jacob Nelson <U46719@ust-global.com>
2018-06-27 18:48:01 +0530

    adding more colours to theme

--- styles/common.js
+++ styles/common.js
@@ -2,7 +2,9 @@ export const primary = '#58C9B9';
 export const secondary = '#111';
 
 export const colours  = {
-  primary: '#226B74',
-  secondary: '#254B5A',
-  tertiary: '#5DA6A7'
+  primary: '#142588',
+  dark: '#254B5A',
+  tertiary: '#5DA6A7',
+  secondary: '#28a745',
+  info: '#226B74'
 }
